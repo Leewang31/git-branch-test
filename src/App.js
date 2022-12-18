8ed8be97d6bae7a10aaaecd2de7ddfa253d7da26
LeeWang <99003659+Leewang31@users.noreply.github.com>
2022-12-18 12:46:12 +0900

    fix2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h4>Hello</h4>
+        <h4>World</h4>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
